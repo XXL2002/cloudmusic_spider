df868d4b56c6845e7ca7048eadcc713bdfda289e
yangkui <1280868105@qq.com>
2023-07-03 20:46:42 +0800

    增加爬取歌手基本信息函数

--- code/singer/get_singer_info.py
+++ code/singer/get_singer_info.py
@@ -3,5 +3,35 @@
 import sys
 sys.path.append("code")
 
-def get_singer_info():
+from tools.request import get
+
+
+def get_singer_info(singer_id):
+
+    url = f'http://music.163.com/api/artist/{singer_id}'
+    data = {}
+    content_json = get(url)
+
+    # 获取歌手名称
+    data['name'] = content_json['artist']['name']
+
+    # 获取歌手用户id
+    data['user_id'] = content_json['artist']['accountId']
     
+    url = 'https://music.163.com/api/v1/user/detail/' + str(data['user_id'])
+    content_json1 = get(url)
+
+    # 获取歌手粉丝
+    data['fans'] = content_json1['profile']['followeds']
+
+    # 获取歌手热门歌曲id
+    data['hotsongs'] = [hotSong['id'] for hotSong in content_json['hotSongs']]
+
+    return data
+
+if __name__=="__main__":
+
+    data = get_singer_info(10559)
+    print(data)
+
+

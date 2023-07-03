4dad556aef27ef1ac61fd7e7a4e5ce5b82ddfb12
yangkui <1280868105@qq.com>
2023-07-03 09:39:05 +0800

    更新用户信息爬取

--- code/get_user_info.py
+++ code/get_user_info.py
@@ -1,7 +1,6 @@
 # 爬取相关数据
 
-import requests
-from utils import headers, city_dic, json2str, user_age
+from utils import city_dic, json2str, user_age, get
 
 
 def get_user(user_id):
@@ -11,46 +10,35 @@ def get_user(user_id):
     """
 
     data = {}
-    url = 'https://music.163.com/api/v1/user/detail/' + str(user_id)
+    url = f'https://music.163.com/api/v1/user/detail/{user_id}'
 
-    response = requests.get(url, headers=headers)
-    content_json = response.json()
+    content_json = get(url)
 
-    # 成功返回
-    try:
+    # 性别
+    if content_json['profile']['gender'] == 1:
+        data['gender'] = '男'
+    elif content_json['profile']['gender'] == 2:
+        data['gender'] = '女'
+    else:
+        data['gender'] = '未知'
 
-        if content_json['code'] == 200:
+    # 年龄
+    if content_json['profile']['birthday'] < 0:     # 时间戳小于0，该用户未填年龄
+        data['age'] = -1
+    else: 
+        data['age'] = user_age(content_json['profile']['birthday'])
 
-            # 性别
-            if content_json['profile']['gender'] == 1:
-                data['gender'] = '男'
-            elif content_json['profile']['gender'] == 2:
-                data['gender'] = '女'
-            else:
-                data['gender'] = '未知'
+    # 省份
+    data['province'] = city_dic[content_json['profile']['province']]
 
-            # 年龄
-            if content_json['profile']['birthday'] < 0:     # 时间戳小于0，该用户未填年龄
-                data['age'] = -1
-            else: 
-                data['age'] = user_age(content_json['profile']['birthday'])
+    # 个人介绍
+    if content_json['profile']['signature'] == "":
+        data['signature'] = "无"
+    else:
+        data['signature'] = content_json['profile']['signature'].replace("\n","").replace("\u200b", "")
 
-            # 省份
-            data['province'] = city_dic[content_json['profile']['province']]
 
-            # 个人介绍
-            if content_json['profile']['signature'] == "":
-                data['signature'] = "无"
-            else:
-                data['signature'] = content_json['profile']['signature'].replace("\n","").replace("\u200b", "")
-
-            response.close()
-
-            return data
-    
-    except:
-
-        print("爬取失败!")
+    return data
         
 
 

b18a20708483f145289ddf5c8d7403a49cb1b282
yangkui <1280868105@qq.com>
2023-07-03 09:31:35 +0800

    上传获取歌词

--- code/get_song_lyric.py
+++ code/get_song_lyric.py
@@ -1,6 +1,7 @@
 from utils import get
 import re
 
+# 根据歌曲ID获取歌词，每句歌词之间以逗号间隔
 def get_song_lyric(songid):
 
     url = f'https://music.163.com/api/song/lyric?id={songid}&lv=1&kv=1&tv=-1'
@@ -9,7 +10,7 @@ def get_song_lyric(songid):
     lyric = content_json['lrc']['lyric'].replace("\n", "")
     pattern = r"\[[^\]]*\]"
 
-    return re.sub(pattern, " ", lyric)
+    return re.sub(pattern, " ", lyric).lstrip()
 
 
 if __name__ == "__main__":

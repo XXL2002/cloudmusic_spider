80e894b7e2ec6da4c9a4d24aa0b460743d29fa41
yangkui <1280868105@qq.com>
2023-07-04 13:10:53 +0800

    删除不必要的的代码

--- code/song/get_song_info.py
+++ code/song/get_song_info.py
@@ -5,18 +5,13 @@ sys.path.append("code")
 from tools.file import save_csv
 from tools.request import get
 from song.get_song_lyric import get_song_lyric
+from tools.struct import file_info_paths
 
 
 def get_song_info(songid):
     '''
         获取歌曲基本信息
     '''
-
-    filename = f"song_info"
-    filepath = f"data/{filename}.txt"
-    
-    # with open(filepath, 'a', encoding='utf-8') as file:
-    #     file.write("{},{},{},{}\n".format("songname", "songer", "album", "lyric"))
     
     url = f'https://music.163.com/api/song/detail/?id={songid}&ids=[{songid}]'
     content_json = get(url)
@@ -30,15 +25,18 @@ def get_song_info(songid):
         # 获取歌手名(只获取第一个)
         data['songer'] = song['artists'][0]['name']
 
+        # 获取歌手ID
+        data['singer_id'] = song['artists'][0]['id']
+        
         # 获取所属专辑
         data['album'] = song['album']['name']
 
         # 获取歌词
         data['lyric'] = get_song_lyric(songid)
 
-        save_csv(filepath, data)
+        save_csv(file_info_paths['song'], data)
         
-        return
+        return data['singer_id']
 
 
 if __name__ == "__main__":

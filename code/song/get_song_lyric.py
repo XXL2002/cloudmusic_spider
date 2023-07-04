0e41fe6b68cb1d9cd5cb3325b106243299afc84a
yangkui <1280868105@qq.com>
2023-07-04 13:57:39 +0800

    增加筛选过程

--- code/song/get_song_lyric.py
+++ code/song/get_song_lyric.py
@@ -16,12 +16,21 @@ def get_song_lyric(songid):
     content_json = get(url)
 
     lyric = content_json['lrc']['lyric'].replace("\n", "")
-    pattern = r"\[[^\]]*\]"
+    pattern1 = r"\[[^\]]*\]"
+    lyric = re.sub(pattern1, " ", lyric).lstrip()
+    pattern2 = r"作词\s*:\s*\w+\s*"
+    lyric = re.sub(pattern2, "", lyric)
+    pattern3 = r"作曲\s*:\s*\w+\s*"
+    lyric = re.sub(pattern3, "", lyric)
+    pattern4 = r'制作人\s*:\s*\w+\s*'
+    lyric = re.sub(pattern4, "", lyric)
+    pattern4 = r'编曲\s*:\s*\w+\s*'
+    lyric = re.sub(pattern4, "", lyric)
 
-    return re.sub(pattern, " ", lyric).lstrip()
+    return lyric
 
 
 if __name__ == "__main__":
 
-    lyric = get_song_lyric(1959190717)
+    lyric = get_song_lyric(2057534370)
     print(lyric)

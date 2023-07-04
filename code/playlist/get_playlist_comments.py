d536f5fcf31c67622a76f513dad3001709584bfd
XXL2002 <1981349981@qq.com>
2023-07-04 10:16:04 +0800

    修改歌单评论

--- code/playlist/get_playlist_comments.py
+++ code/playlist/get_playlist_comments.py
@@ -8,10 +8,12 @@ from tools.request import get
 from tools.comment import hotcomments, comments
 
 
-def get_playlist_comments(songname, songid, filepath):
+def get_playlist_comments(playlistid):
     '''
         获取歌单的评论
     '''
+    filename = f"playlist_{playlistid}_comments"
+    filepath = f"data/{filename}.txt"
 
     with open(filepath, 'a', encoding='utf-8') as file:
         file.write("{},{},{},{},{},{},{}\n".format("user_id", "user_name", "comment_id", "comment", "time", "likecount", "location"))
@@ -20,7 +22,7 @@ def get_playlist_comments(songname, songid, filepath):
 
     page = 0    # 第一页
     
-    url = f'https://music.163.com/api/v1/resource/comments/A_PL_0_{songid}?limit=20&offset={page}'
+    url = f'https://music.163.com/api/v1/resource/comments/A_PL_0_{playlistid}?limit=20&offset={page}'
 
     # 获取第一页评论，json格式
     content_json = get(url)
@@ -33,20 +35,20 @@ def get_playlist_comments(songname, songid, filepath):
 
     print("总共有{}页{}条评论\n".format(pages, total))
 
-    hotcomments(content_json, songname, page, filepath)
-    comments(content_json, songname, page, filepath)
+    hotcomments(content_json, filename, page, filepath)
+    comments(content_json, filename, page, filepath)
 
     # 开始获取歌曲的全部评论
     page = 1
 
-    # 取50个页
-    while page < 50:
+    # 爬取所有评论
+    while page < pages:
 
-        url = f'https://music.163.com/api/v1/resource/comments/A_PL_0_{songid}?limit=20&offset={page}'
+        url = f'https://music.163.com/api/v1/resource/comments/A_PL_0_{playlistid}?limit=20&offset={page}'
         content_json = get(url)
 
         # 从第二页开始获取评论
-        comments(content_json, songname, page, filepath)
+        comments(content_json, filename, page, filepath)
         page += 1
     
     print("爬取结束!")
@@ -55,6 +57,6 @@ def get_playlist_comments(songname, songid, filepath):
 
 if __name__ == "__main__":
 
-    filename = "playlist_comments"
-    filepath = f"data\{filename}.txt"
-    get_playlist_comments(filename, 2452136763, filepath)
+    # filename = "playlist_comments"
+    # filepath = f"data\{filename}.txt"
+    get_playlist_comments(8421225458)

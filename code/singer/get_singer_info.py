adc6f39e76d564deb6ed2b82e2cd9b0f3339303d
yangkui <1280868105@qq.com>
2023-07-04 17:17:17 +0800

    更新

--- code/singer/get_singer_info.py
+++ code/singer/get_singer_info.py
@@ -16,22 +16,30 @@ def get_singer_info(singer_id):
     data = {}
     content_json = get(url)
 
-    # 获取歌手用户id
-    data['singer_id'] = content_json['artist']['accountId']
-
-    # 获取歌手名称
-    data['singer_name'] = content_json['artist']['name']
-    
-    url = 'https://music.163.com/api/v1/user/detail/' + str(data['singer_id'])
-    content_json1 = get(url)
+    # 歌手ID
+    data['singer_id'] = singer_id
 
     if 'accountId' in content_json['artist']:
+
+        # 获取歌手用户id
+        data['accountId'] = content_json['artist']['accountId']
+
+        url = 'https://music.163.com/api/v1/user/detail/' + str(data['accountId'])
+        content_json1 = get(url)
 
         # 获取歌手粉丝
         data['fans'] = content_json1['profile']['followeds']
 
+
     else:
+
+        data['accountId'] = 0
         data['fans'] = 0
+
+
+    # 获取歌手名称
+    data['singer_name'] = content_json['artist']['name']
+
 
     # 获取歌手热门歌曲id
     hotSongs = [hotSong['id'] for hotSong in content_json['hotSongs']]
@@ -41,9 +49,10 @@ def get_singer_info(singer_id):
 
     return data
 
+
 if __name__=="__main__":
 
-    data = get_singer_info(51849113)
+    data = get_singer_info(10559)
     print(data)
 
 
--- code/tools/struct.py
+++ code/tools/struct.py
@@ -114,7 +114,7 @@ Music_charts = {
 # 文件头
 file_headers = {
     'song': ["songname", "songer", "singer_id", "album", "lyric"],
-    'singer': ["singer_id", "singer_name", "fans", "hotsongs"],
+    'singer': ["singer_id", "accountId", "fans", "singer_name", "hotsongs"],
     'playlist': ["playlist_id", "playlist_name", "playCount", "subscribedCount", "description", "tags", "creator", "trackIds"],
     'comment': ["user_id", "user_name", "comment_id", "comment", "time", "likecount", "location"],
     'user': ["nickname", "gender", "age", "province", "signature"]

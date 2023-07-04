03fef63b409b7230269d001031c15a32dc1e8ea0
yangkui <1280868105@qq.com>
2023-07-04 22:01:22 +0800

    增加cleardir

--- code/tools/file.py
+++ code/tools/file.py
@@ -1,5 +1,6 @@
-import openpyxl
-
+# import openpyxl
+import shutil
+import os
 
 # 将json格式的值列表转换为逗号分割的字符串
 def json2str(data):
@@ -10,7 +11,11 @@ def json2str(data):
 # 向指定文件添加文件头
 def add_header(filepath, header):
     with open(filepath, 'a', encoding='utf-8') as file:
+        # file.seek(0)  # 定位到文件起始位置
+        # file.truncate()     # 清空文件内容
         file.write(','.join([str(i) for i in header]) + "\n")
+    
+    file.close()
 
 
 # 将json格式值数据写入csv文件
@@ -22,19 +27,32 @@ def save_csv(path, data):
     f.close()
     
 
-
-def create_xlsx(path, header):
-
-    # 创建工作簿
-    workbook = openpyxl.Workbook()
-
-    # 获取表
-    sheet = workbook.active
-
-    # 设置表头
-    sheet.append(header)
+# 清空非空目录
+def cleardir(path):
+    if os.path.exists(path):    # 目录已存在
+        shutil.rmtree(path)
+    os.mkdir(path)
     
-    # 保存文件
-    workbook.save(path)
 
-    return sheet
+
+# def create_xlsx(path, header):
+
+#     # 创建工作簿
+#     workbook = openpyxl.Workbook()
+
+#     # 获取表
+#     sheet = workbook.active
+
+#     # 设置表头
+#     # sheet.append(header)
+    
+#     # 保存文件
+#     workbook.save(path)
+
+#     return sheet
+
+
+if __name__=="__main__":
+
+    path = r'data\info'
+    rmdir(path)

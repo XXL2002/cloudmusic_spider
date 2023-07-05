445599bcbcd6978932ff0b03ed6a5a79c7ed98a2
yangkui <1280868105@qq.com>
2023-07-05 09:57:58 +0800

    上传本地文件到hdfs

--- code/process/update.py
+++ code/process/update.py
@@ -1,3 +1,51 @@
+# 先安装simplejson，再安装pyhdfs
 from pyhdfs import HdfsClient
-client = HdfsClient(hosts='stu:50070',user_name='root')
-client.copy_from_local('/home/hadoop1/software/data/one.txt','/1')#本地文件绝对路径,HDFS目录必须不存在
+import os
+
+client = HdfsClient(hosts='stu:50070', user_name='root')
+
+path = '/home/cloudmusic_spider/data/'
+remote_path = '/data/'
+
+info_path = path + 'info/'  # info目录路径
+playlist_comments_path = path + 'playlist_comments/' # playlist_comments目录路径
+song_comments_path = path + 'song_comments/' # song_comments目录路径
+
+remote_info_path = remote_path + 'info/'  # info目录路径
+remote_playlist_comments_path = remote_path + 'playlist_comments/' # playlist_comments目录路径
+remote_song_comments_path = remote_path + 'song_comments/' # song_comments目录路径
+
+if client.exists(remote_info_path):
+    client.delete(remote_info_path, recursive=True)
+
+client.mkdirs(remote_info_path)
+
+if client.exists(remote_playlist_comments_path):
+    client.delete(remote_playlist_comments_path, recursive=True)
+
+client.mkdirs(remote_playlist_comments_path)
+
+if client.exists(remote_song_comments_path):
+    client.delete(remote_song_comments_path, recursive=True)
+
+client.mkdirs(remote_song_comments_path)
+
+
+print('开始上传文件...')
+
+for file in os.listdir(info_path):
+    path = info_path + file
+    hdfs_path = remote_info_path + file
+    client.copy_from_local(path, hdfs_path)
+
+for file in os.listdir(playlist_comments_path):
+    path = playlist_comments_path + file
+    hdfs_path = remote_playlist_comments_path + file
+    client.copy_from_local(path, hdfs_path)
+
+for file in os.listdir(song_comments_path):
+    path = song_comments_path + file
+    hdfs_path = remote_song_comments_path + file
+    client.copy_from_local(path, hdfs_path)   
+
+print('上传完毕')

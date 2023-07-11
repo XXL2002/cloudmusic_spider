1c12c917f81507f96a79a5f95468f5b0b9ba7840
XXL2002 <1981349981@qq.com>
2023-07-11 21:50:18 +0800

    ！处理406等异常&retry操作

--- code/tools/request.py
+++ code/tools/request.py
@@ -1,13 +1,19 @@
 import sys
+# import time
 sys.path.append("code")
-
+from tools.sleep import sleep
 import requests
 from tools.struct import get_header
 
 
 # get请求，返回json数据
-def get(url):
+def get(url,retry = 5):
 
+    if retry != 5:
+        sleep(5)
+    if retry == 0:
+        print("retry overflows!")
+        raise Exception("retry overflows!")
     try:
         response = requests.get(url, headers=get_header())
         content_json = response.json()
@@ -18,14 +24,25 @@ def get(url):
             response.close()
             return content_json
         
+        else:
+            
+            print(content_json['code'])
+            return get(url,retry=retry-1)
+        
     except:
 
         print("爬取失败!")
     
 
 # post请求
-def post(url, data):
+def post(url, data,retry = 5):
 
+    if retry != 5:
+        time.sleep(30)
+    if retry == 0:
+        print("retry overflows!")
+        raise Exception("retry overflows!")
+    
     try:
         response = requests.post(url, headers=get_header(), data=data)
         content_json = response.json()
@@ -34,6 +51,11 @@ def post(url, data):
 
             response.close()
             return content_json
+        
+        else:
+            
+            print(content_json['code'])
+            return post(url,data,retry=retry-1)
         
     except:
 

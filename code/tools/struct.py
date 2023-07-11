1ecbcc32bd78b4e404d12dcf5f6cf2c14acce5fa
XXL2002 <1981349981@qq.com>
2023-07-11 21:51:12 +0800

    改用fake_useragent生成User-Agent

--- code/tools/struct.py
+++ code/tools/struct.py
@@ -1,6 +1,8 @@
 import random
+from fake_useragent import UserAgent
 # 伪造请求头
-headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             # 'Origin': 'https://music.163.com',
             # 'Referer': 'https://music.163.com/playlist?id=750115024',
             'Accept-Encoding': 'gzip, deflate',
@@ -11,20 +13,26 @@ headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/
             'Referer': 'http://music.163.com/',
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
-
 # 使用随机User-Agent
 User_Agent = {0:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
               1:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-              2:"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
-              3:"Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36",
-              4:"Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1",
-              5:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36",
+            #   2:"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+            #   3:"Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36",
+            #   4:"Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1",
+              2:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36",
               }
 
 # 获取随机生成的请求头
-def get_header():
-    agent_idx = random.randint(0, 5)
-    headers['User-Agent'] = User_Agent[agent_idx]
+def get_header(idx = -1):
+    # agent_idx = random.randint(0, 2)
+    # if idx != -1:
+    #     agent_idx = idx
+    # print(f"now using=={agent_idx}")
+    # headers['User-Agent'] = User_Agent[agent_idx]
+    
+    # 改用fake_useragent
+    headers['User-Agent'] = str(UserAgent().random)
+    # print(headers['User-Agent'])
     return headers
 
 # 城市字典

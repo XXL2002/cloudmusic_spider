00b221475965e2a543b39d6fc80068ea0e1c75d2
XXL2002 <1981349981@qq.com>
2023-07-11 21:50:42 +0800

    晚上正态sleep算子

--- code/tools/sleep.py
+++ code/tools/sleep.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-def sleep():
-    mu = 0.5  # 平均值
+def sleep(mu = 0.5):
+    # mu = 0.5  # 平均值
     sigma = 0.5  # 标准差
     # 正态分布
     sleep_time = random.gauss(mu, sigma)
-    sleep_time = max(0.2, min(3, sleep_time))  # 将休眠时间限制在1到3之间
+    sleep_time = max(0.5*mu, min(1.5*mu, sleep_time))  # 将休眠时间限制在0.5x到1.5x之间
 
     time.sleep(sleep_time)

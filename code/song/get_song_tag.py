46ff9bc9f0976ca05fed10631f739dae0d139c69
XXL2002 <1981349981@qq.com>
2023-07-11 21:48:16 +0800

    finish get_song_tag with xpath

---
+++ code/song/get_song_tag.py
@@ -0,0 +1,57 @@
+import sys
+import time
+import requests
+from lxml import etree
+sys.path.append("code")
+from tools.struct import get_header
+from tools.request import get
+from tools.sleep import sleep
+
+def get_playlist_tag(playlistid):
+    '''
+        获取指定歌单的标签
+    '''
+    print(f"开始爬取歌单{playlistid}的tags...")
+    url = f'https://music.163.com/api/v1/playlist/detail?id={playlistid}'
+
+    content_json = get(url)
+
+    if content_json is None:
+        print("11111")
+        return ""
+        
+    # 返回歌单标签列表
+    print(content_json['playlist']['tags'])
+    return content_json['playlist']['tags']
+
+
+def get_song_tag(song_id):
+
+    try:
+        url = f"https://music.163.com/song?id={song_id}"
+        response = requests.get(url, headers=get_header())
+        if response.status_code == 200:
+            response.close()
+    except:
+        print("爬取失败!")
+        
+    html = etree.HTML(response.text)
+    # time.sleep(1)
+    # list
+    html_data = html.xpath("//div[@class='info']//@data-res-id")
+    html_data = [int(i) for i in html_data]
+    print(html_data)
+    tags = []
+    for id in html_data:
+        sleep(1.5)
+        tags += get_playlist_tag(id)
+    # 去重
+    tags = list(set(tags))
+    # str
+    res = str(" ".join(tags))
+    print(res)
+    return res
+        
+
+if __name__ == "__main__":
+    get_song_tag(1964443044)
